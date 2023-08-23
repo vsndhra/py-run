b632615f4110a2f95ea4d58cebeb181d287532b4
transposter@908 <vasundhara13001@gmail.com>
2023-08-23 10:04:41 +0530

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import subprocess
 import re
 
 app = Flask(__name__)
+CORS(app) 
 
 def execute_code_safely(code):
     try:
